29c30b5d5250a3cedd7d2619939e37f66b7d9e5a
Ctfurlani <38194317+Ctfurlani@users.noreply.github.com>
2020-10-05 18:12:19 -0300

    Etapa 2 sem movimentação
    Sem movimentação automática, mapeamento está incorreto ainda

--- Robot.cpp
+++ Robot.cpp
@@ -1,353 +1,180 @@
-#include "Robot.h"
-
+#include <cstdio>
 #include <GL/glut.h>
-#include <cmath>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <float.h> // DBL_MAX
 
-//////////////////////////////////////
-///// CONSTRUCTORS & DESTRUCTORS /////
-//////////////////////////////////////
+#include "Grid.h"
+#include "math.h"
 
-Robot::Robot()
+Grid::Grid ()
 {
-    ready_ = false;
-    running_ = true;
+    mapScale_ = 10;
+    mapWidth_ = mapHeight_ = 2000;
+    numCellsInRow_=mapWidth_;
+    halfNumCellsInRow_=mapWidth_/2;
 
-    grid = new Grid();
+    cells_ = new Cell[mapWidth_*mapHeight_];
 
-    // variables used for navigation
-    isFollowingLeftWall_=false;
-
-    // variables used for visualization
-    viewMode=1;
-    numViewModes=5;
-}
-
-Robot::~Robot()
-{
-    base.closeARIAConnection();
-    if(grid!=NULL)
-        delete grid;
-}
-
-////////////////////////////////////
-///// INITIALIZE & RUN METHODS /////
-////////////////////////////////////
-
-void Robot::initialize(ConnectionMode cmode, LogMode lmode, std::string fname)
-{
-    logMode_ = lmode;
-//    logFile_ = new LogFile(logMode_,fname);
-    ready_ = true;
-
-    // initialize ARIA
-    if(logMode_!=PLAYBACK){
-        bool success = base.initialize(cmode,lmode,fname);
-        if(!success){
-            printf("Could not connect to robot... exitingn");
-            exit(0);
-        }
-    }
-
-    ready_ = true;
-    controlTimer.startLap();
-}
-
-void Robot::run()
-{
-    controlTimer.waitTime(0.1);
-
-    if(logMode_==PLAYBACK){
-        bool hasEnded = base.readFromLog();
-        if(hasEnded){
-            std::cout << "PROCESS COMPLETE. CLOSING PROGRAM." << std::endl;
-            exit(0);
-        }
-    }else{
-        bool success = base.readOdometryAndSensors();
-        if(!success){
-            usleep(50000);
-            return;
-        }
-
-        if(logMode_==RECORDING)
-            base.writeOnLog();
-    }
-
-    currentPose_ = base.getOdometry();
-
-    // Save path traversed by the robot
-    if(base.isMoving() || logMode_==PLAYBACK){
-        path_.push_back(base.getOdometry());
-    }
-
-    // Navigation
-    switch(motionMode_){
-        case WANDER:
-            wanderAvoidingCollisions();
-            break;
-        case WALLFOLLOW:
-            wallFollow();
-            break;
-        case ENDING:
-            running_=false;
-            break;
-        default:
-            break;
-    }
-
-    base.resumeMovement();
-
-    usleep(50000);
-}
-
-//////////////////////////////
-///// NAVIGATION METHODS /////
-//////////////////////////////
-
-void Robot::move(MovingDirection dir)
-{
-    switch(dir){
-        case FRONT:
-            std::cout << "moving front" << std::endl;
-            break;
-        case BACK:
-            std::cout << "moving back" << std::endl;
-            break;
-        case LEFT:
-            std::cout << "turning left" << std::endl;
-            break;
-        case RIGHT:
-            std::cout << "turning right" << std::endl;
-            break;
-        case STOP:
-            std::cout << "stopping robot" << std::endl;
-    }
-
-    if(motionMode_==MANUAL_SIMPLE)
-        base.setMovementSimple(dir);
-    else if(motionMode_==MANUAL_VEL)
-        base.setMovementVel(dir);
-    else if(motionMode_=WALLFOLLOW)
-        if(dir==LEFT)
-            isFollowingLeftWall_=true;
-        else if(dir==RIGHT)
-            isFollowingLeftWall_=false;
-}
-
-void Robot::wanderAvoidingCollisions()
-{
-    //TODO - implementar desvio de obstaculos
-    float minLeftSonar  = base.getMinSonarValueInRange(0,2);
-    float minFrontSonar = base.getMinSonarValueInRange(3,4);
-    float minRightSonar = base.getMinSonarValueInRange(5,7);
-
-    float minLeftLaser  = base.getMinLaserValueInRange(0,74);
-    float minFrontLaser = base.getMinLaserValueInRange(75,105);
-    float minRightLaser = base.getMinLaserValueInRange(106,180);
-
-    float linVel=0;
-    float angVel=0;
-
-   //TODO - implementar desvio de obstaculos
-   float min_distance = 1.0;
-   std::cout << minLeftSonar << " " <<  minLeftLaser<< "n";
-   std::cout << minFrontSonar << " " <<  minFrontLaser<< "n";
-   std::cout << minRightSonar << " " <<  minRightLaser<< "n";
-
-   if (minFrontLaser <= min_distance || minFrontSonar <= min_distance){ // Obstacle in front
-       if ((minLeftLaser < minRightLaser) || (minLeftSonar < minRightSonar)){
-           linVel = 0.0;
-           angVel = -1;
-       }
-       else{
-           linVel = 0.0;
-           angVel = 1;
-       }
-   }
-   else{
-       if (minLeftLaser <= min_distance || minLeftSonar <= min_distance){
-           linVel = 20;
-           angVel = -1;
-       }
-       else if (minRightLaser <= min_distance || minRightSonar <= min_distance){
-           linVel = 20;
-           angVel = 1;
-       }
-       else{
-           linVel = 20;
-           angVel = 0;
-       }
-
-   }
-
-    base.setWheelsVelocity_fromLinAngVelocity(linVel, angVel);
-}
-
-void Robot::wallFollow()
-{
-    float minLeftSonar  = base.getMinSonarValueInRange(0,2);
-    float minFrontSonar = base.getMinSonarValueInRange(3,4);
-    float minRightSonar = base.getMinSonarValueInRange(5,7);
-
-    float minLeftLaser  = base.getMinLaserValueInRange(0,74);
-    float minFrontLaser = base.getMinLaserValueInRange(75,105);
-    float minRightLaser = base.getMinLaserValueInRange(106,180);
-
-    float linVel=0.5;
-    float angVel=0;
-
-    if(isFollowingLeftWall_)
-        std::cout << "Following LEFT wall" << std::endl;
-    else
-        std::cout << "Following RIGHT wall" << std::endl;
-
-    //TODO - implementar wall following usando PID
-    float tp = 1.5;
-    float td = 11.5;
-    float ti = 0.0001;
-    float CTE;
-    float SP = 1.0;
-    static std::vector<float> vec_CTE;
-    //static float prev_CTE = 0;
-
-    if(isFollowingLeftWall_){
-        CTE = minLeftLaser-SP;
-        CTE = -CTE;
-        if(m_wallFollowState == true){
-            m_wallFollowState = false;
-            m_prev_CTE = 0;
-            vec_CTE.clear();
-        }
-    }else{
-        CTE = minRightLaser-SP;
-        if(m_wallFollowState == false){
-            m_wallFollowState = true;
-            m_prev_CTE = 0;
-            vec_CTE.clear();
-        }
-    }
-
-    float deriv_CTE = CTE - m_prev_CTE;
-    float sum_CTE=0;
-    vec_CTE.push_back(CTE);
-    if (vec_CTE.size() > 50){
-        vec_CTE.erase(vec_CTE.begin());
-    }
-    for (std::vector<float>::iterator it = vec_CTE.begin(); it != vec_CTE.end(); ++it){
-        sum_CTE += *it;
-    }
-    std::cout << "CTE: " << CTE << " prev: " << m_prev_CTE << " sum: " << sum_CTE<< std::endl;
-    m_prev_CTE = CTE;
-    angVel = -tp*CTE - td*deriv_CTE - ti*sum_CTE;
-    std::cout << "Ang Vel: " << angVel << std::endl;
-
-    if(minFrontLaser < 0.8){
-        linVel = 0.01;
-    }else{
-        linVel = 0.5;
-    }
-
-
-    base.setWheelsVelocity_fromLinAngVelocity(linVel, angVel);
-}
-
-/////////////////////////////////////////////////////
-////// METHODS FOR READING & WRITING ON LOGFILE /////
-/////////////////////////////////////////////////////
-
-// Prints to file the data that we would normally be getting from sensors, such as the laser and the odometry.
-// This allows us to later play back the exact run.
-void Robot::writeOnLog()
-{
-    logFile_->writePose("Odometry",currentPose_);
-    logFile_->writeSensors("Sonar",base.getSonarReadings());
-    logFile_->writeSensors("Laser",base.getLaserReadings());
-}
-
-// Reads back into the sensor data structures the raw readings that were stored to file
-// While there is still information in the file, it will return 0. When it reaches the end of the file, it will return 1.
-bool Robot::readFromLog() {
-
-    if(logFile_->hasEnded())
-        return true;
-
-    base.setOdometry(logFile_->readPose("Odometry"));
-    base.setSonarReadings(logFile_->readSensors("Sonar"));
-    base.setLaserReadings(logFile_->readSensors("Laser"));
-
-    return false;
-}
-
-////////////////////////
-///// DRAW METHODS /////
-////////////////////////
-
-void Robot::draw(float xRobot, float yRobot, float angRobot)
-{
-    float scale = grid->getMapScale();
-    glTranslatef(xRobot,yRobot,0.0);
-    glRotatef(angRobot,0.0,0.0,1.0);
-
-    glScalef(1.0/scale,1.0/scale,1.0/scale);
-
-    // sonars and lasers draw in cm
-    if(viewMode==1)
-        base.drawSonars(true);
-    else if(viewMode==2)
-        base.drawSonars(false);
-    else if(viewMode==3)
-        base.drawLasers(true);
-    else if(viewMode==4)
-        base.drawLasers(false);
-
-    // robot draw in cm
-    base.drawBase();
-
-    glScalef(scale,scale,scale);
-    glRotatef(-angRobot,0.0,0.0,1.0);
-    glTranslatef(-xRobot,-yRobot,0.0);
-}
-
-void Robot::drawPath()
-{
-    float scale = grid->getMapScale();
-
-    if(path_.size() > 1){
-        glScalef(scale,scale,scale);
-        glLineWidth(3);
-        glBegin( GL_LINE_STRIP );
+    for (unsigned int j = 0; j < numCellsInRow_; ++j)
+    {
+        for (unsigned int i = 0; i < numCellsInRow_; ++i)
         {
-            for(unsigned int i=0;i<path_.size()-1; i++){
-                glColor3f(1.0,0.0,1.0);
+            unsigned int c = j*numCellsInRow_ + i;
+            cells_[c].x = -halfNumCellsInRow_ + 1 + i;
+            cells_[c].y =  halfNumCellsInRow_ - j;
 
-                glVertex2f(path_[i].x, path_[i].y);
-                glVertex2f(path_[i+1].x, path_[i+1].y);
+            cells_[c].occupancy = 0.5;
+            cells_[c].logodds = 0;
+            cells_[c].occupancySonar = 0.5;
+            cells_[c].logoddsSonar = 0;
+            cells_[c].himm = 7;
+
+            cells_[c].occType = UNEXPLORED;
+            cells_[c].planType = REGULAR;
+
+            cells_[c].dirX=0.0;
+            cells_[c].dirY=0.0;
+        }
+    }
+
+    numViewModes=4;
+    viewMode=0;
+
+    showValues=false;
+    showArrows=false;
+}
+
+Cell* Grid::getCell (int x, int y)
+{
+    int i=x+halfNumCellsInRow_-1;
+    int j=halfNumCellsInRow_-y;
+    return &(cells_[j*numCellsInRow_ + i]);
+}
+
+int Grid::getMapScale()
+{
+    return mapScale_;
+}
+
+int Grid::getMapWidth()
+{
+    return mapWidth_;
+}
+
+int Grid::getMapHeight()
+{
+    return mapHeight_;
+}
+
+void Grid::draw(int xi, int yi, int xf, int yf)
+{
+    glLoadIdentity();
+
+    for(int i=xi; i<=xf; ++i){
+        for(int j=yi; j<=yf; ++j){
+            drawCell(i+j*numCellsInRow_);
+        }
+    }
+
+    if(showArrows){
+        glPointSize(2);
+        for(int i=xi; i<=xf; ++i){
+            for(int j=yi; j<=yf; ++j){
+                drawVector(i+j*numCellsInRow_);
             }
         }
-        glEnd();
-        glLineWidth(1);
-        glScalef(1.0/scale,1.0/scale,1.0/scale);
+    }
 
+    if(showValues){
+        for(int i=xi; i<=xf; i++){
+            for(int j=yi; j<=yf; j++){
+                drawText(i+j*numCellsInRow_);
+            }
+        }
     }
 }
 
-/////////////////////////
-///// OTHER METHODS /////
-/////////////////////////
-
-bool Robot::isReady()
+void Grid::drawCell(unsigned int n)
 {
-    return ready_;
+    float aux;
+
+    if(viewMode==0){
+        // DRAW OCCUPANCY GRID MAP
+        aux=(1.0-cells_[n].occupancy);
+        glColor3f(aux,aux,aux);
+    }else if(viewMode==1){
+        // DRAW SONAR OCCUPANCY GRID MAP
+        aux=(1.0-cells_[n].occupancySonar);
+        glColor3f(aux,aux,aux);
+    }else if(viewMode==2){
+        // DRAW HIMM GRID MAP
+        aux=(16.0-cells_[n].himm)/16.0;
+        glColor3f(aux,aux,aux);
+    }else if(viewMode==3){
+        // DRAW CLASSIFIED CELLS (FROM PLANNING)
+        if(cells_[n].occType == FREE){
+            if(cells_[n].planType == DANGER){
+                glColor3f(1.0,0.0,0.0);
+            }else{
+                glColor3f(1.0,1.0,0.7);
+            }
+        }else if(cells_[n].occType == UNEXPLORED){
+            if(cells_[n].planType == FRONTIER){
+                glColor3f(0.0,0.8,0.3);
+            }else{
+                glColor3f(0.6,0.6,0.6);
+            }
+        }else if(cells_[n].occType == OCCUPIED){
+            glColor3f(0.3,0.0,0.0);
+        }
+    }
+
+    glBegin( GL_QUADS );
+    {
+        glVertex2f(cells_[n].x+1, cells_[n].y+1);
+        glVertex2f(cells_[n].x+1, cells_[n].y  );
+        glVertex2f(cells_[n].x  , cells_[n].y  );
+        glVertex2f(cells_[n].x  , cells_[n].y+1);
+    }
+    glEnd();
 }
 
-bool Robot::isRunning()
+void Grid::drawVector(unsigned int n)
 {
-    return running_;
+    if(cells_[n].occType == FREE){
+        if(cells_[n].dirX == 0.0 && cells_[n].dirY == 0.0)
+            glColor3f(1.0,0.7,0.0);
+        else
+            glColor3f(1.0,0.0,0.0);
+        glLineWidth(1);
+        glBegin( GL_LINES );
+        {
+            glVertex2f(cells_[n].x+0.5, cells_[n].y+0.5);
+            glVertex2f(cells_[n].x+0.5+cells_[n].dirX, cells_[n].y+0.5+cells_[n].dirY);
+        }
+        glEnd();
+        glBegin( GL_POINTS );
+        {
+            glVertex2f(cells_[n].x+0.5+cells_[n].dirX, cells_[n].y+0.5+cells_[n].dirY);
+        }
+        glEnd();
+    }
 }
 
-const Pose& Robot::getCurrentPose()
+void Grid::drawText(unsigned int n)
 {
-    return currentPose_;
-}
+    glRasterPos2f(cells_[n].x+0.25, cells_[n].y+0.25);
+    std::stringstream s;
+    glColor3f(0.5f, 0.0f, 0.0f);
+//    s << std::setprecision(1) << std::fixed << cells_[n].pot;
+    s << cells_[n].himm;
 
+
+    std::string text=s.str();
+    for (unsigned int i=0; i<text.size(); i++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10, text[i]);
+    }
+}
